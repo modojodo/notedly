08101fad29d09c2e0510c97e705250fc6084fa85
Umer Hassan <umerhassan94@gmail.com>
2020-09-22 00:14:56 -0400

    importing graphql, adding type definition and resolver

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000;
+const { ApolloServer, gql } = require('apollo-server-express');
 
+const port = process.env.PORT || 4000;
+const typeDefs = gql`
+type Query {
+  hello: String
+}
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => `Hello World`,
+  }
+}
 app.get('/', (req, res) => res.send('Hello World'));
+
 app.listen(port, () => console.log('Listening on port 4000!'));
