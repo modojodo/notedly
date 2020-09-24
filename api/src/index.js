b591fe0db4d595184ef704f100c11db0fa1b5190
Umer Hassan <umerhassan94@gmail.com>
2020-09-23 22:31:41 -0400

    Apollo server setup and some stub data to play with

--- api/src/index.js
+++ api/src/index.js
@@ -3,6 +3,12 @@ const app = express();
 const { ApolloServer, gql } = require('apollo-server-express');
 
 const port = process.env.PORT || 4000;
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
 const typeDefs = gql`
 type Query {
   hello: String
@@ -14,6 +20,14 @@ const resolvers = {
     hello: () => `Hello World`,
   }
 }
-app.get('/', (req, res) => res.send('Hello World'));
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.listen(port, () => console.log('Listening on port 4000!'));
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
